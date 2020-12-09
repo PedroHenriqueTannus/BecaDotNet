07e8660ee941f479358c15db2638a3c4884ab441
Diego Prestes <diegoprestes.git@hotmail.com>
2020-12-09 17:09:21 -0300

    fix: merge fix

--- BecaDotNet/Bubblesort.cs
+++ BecaDotNet/Bubblesort.cs
@@ -6,8 +6,43 @@ using System.Threading.Tasks;
 
 namespace BecaDotNet
 {
-    public class BubbleSort
+    public class BubbleSortAlg
     {
+
+        public void OrdenaListaDeInteiros(int[] arr)
+        {
+            //declara uma variável que recebe o valor que será trocado
+            int swapValue;
+
+            //realiza um loop for dentro de outro 
+            //para que todos os números sejam comparados entre si
+            for (int j = 0; j < arr.Length - 1; j++)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+
+                    if (arr[i + 1] < arr[i])
+                    {
+                        //troca numeros que estão em sequência 
+                        //caso o segundo seja menor que o primeiro
+                        swapValue = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = swapValue;
+                    }
+                }
+            }
+
+            //realiza um foreach para passar por cada um dos números do array arr
+            foreach (int i in arr)
+            {
+                //escreve cada item i 
+                Console.WriteLine(i + " ");
+            }
+            //le os dados escritos no console
+            Console.Read();
+
+        }
+
         public void LerArray(int[] ordenado)
         {
             for (int i = 0; i < ordenado.Length; i++)
