f756349eb6d4115fad01948d7384a66396d24d82
PedroHenriqueTannus <pedrohenriquetannus@gmail.com>
2020-12-09 14:48:31 -0300

    Teste final

--- BecaDotNet/Program.cs
+++ BecaDotNet/Program.cs
@@ -11,7 +11,7 @@ namespace BecaDotNet
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hellow");
+            Console.WriteLine("Hello world");
         }
     }
 }
