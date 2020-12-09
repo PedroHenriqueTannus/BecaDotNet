d6b794b0ba0420adb3bd28a6cd909603ac156b51
Eduardo <eduardoschlacerda@gmail.com>
2020-12-09 16:27:51 -0300

    Adiconado autor do código

--- BecaDotNet/Program.cs
+++ BecaDotNet/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-
+            //Autor: Eduardo Lacerda
             int[] lista = { 78, 55, 45, 98, 13 };
             BubbleSort.Iniciar(lista);
 
