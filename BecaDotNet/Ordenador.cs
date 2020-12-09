29396299d67494a8e32ab5f3806e2c95e39712c9
Leticia Sorrilha Souza <leticiasorrilhageo@gmail.com>
2020-12-09 15:32:52 -0300

    Implementacao do algoritmo Bubble Sort Aprendizado

---
+++ BecaDotNet/Ordenador.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BecaDotNet
+{
+    class Ordenador
+    {
+        public int[] VetorEntrada { get; set; }
+
+        public int[] BubbleSort()
+        {
+            int[] vet = VetorEntrada;
+            int lenght = vet.Length;
+            int comparacoes = 0;
+            int trocas = 0;
+
+            for (int i = lenght - 1; i >= 1; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    comparacoes++;
+                    if (vet[j] > vet[j + 1])
+                    {
+                        int aux = vet[j];
+                        vet[j] = vet[j + 1];
+                        vet[j + 1] = aux;
+                        trocas++;
+                    }
+                }
+            }
+
+            return vet;
+        }
+    }
+}
--- BecaDotNet/Program.cs
+++ BecaDotNet/Program.cs
@@ -10,8 +10,18 @@ namespace BecaDotNet
     {
         static void Main(string[] args)
         {
+            Ordenador ordenador = new Ordenador();
+            int[] vetEntrada = { 98, 78, 55, 45, 13 };
+            ordenador.VetorEntrada = vetEntrada;
+            int[] vet = ordenador.BubbleSort();
 
-            Console.WriteLine("Hello world");
+            Console.Write("Vetor Ordenado: ");
+            for (int i = 0; i < vet.Length; i++)
+            {
+                Console.Write(vet[i] + ", ");
+            }
+
+            Console.Read();
         }
     }
 }
