58348009b892040349416fff0e7f3bfbc050fc64
Estêvão Cardoso Silva <estevaof10@gmail.com>
2020-12-09 16:10:34 -0300

    Algoritmo de Ordenação BubbleSort

---
+++ BecaDotNet/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BecaDotNet
+{
+    class BubbleSort
+    {
+        // metodo que ordena o vetor 
+        public int[] bbSort(int[] sort)
+        {
+            
+            for (int i = 5 - 1; i >= 1; i--)
+            {
+                for (int n = 0; n < i; n++)
+                {
+                    
+                    if (sort[n] > sort[n + 1])
+                    {
+                        int val = sort[n];
+                        sort[n] = sort[n + 1];
+                        sort[n + 1] = val;
+
+                    }
+                }
+            }
+            return sort;
+        }
+    }
+}
