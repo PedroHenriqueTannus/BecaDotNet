ed899abcbcf828db82c71a3b7d10458bd008b446
Leticia Sorrilha Souza <leticiasorrilhageo@gmail.com>
2020-12-09 18:00:22 -0300

    Ajustes conforme Solicitação do Revisor

--- BecaDotNet/Ordenador.cs
+++ BecaDotNet/Ordenador.cs
@@ -15,20 +15,17 @@ namespace BecaDotNet
         {
             int[] vet = VetorEntrada;// Recebe vetor de entrada.
             int lenght = vet.Length;
-            int comparacoes = 0;
-            int trocas = 0;
-
+            
             for (int i = lenght - 1; i >= 1; i--)
             {
                 for (int j = 0; j < i; j++)
                 {
-                    comparacoes++;
+                    
                     if (vet[j] > vet[j + 1])
                     {
                         int aux = vet[j];
                         vet[j] = vet[j + 1];
                         vet[j + 1] = aux;
-                        trocas++;
                     }
                 }
             }
