79e98c1d02f689297c3f4859236103b93d1bdf4e
GRZMT <guii.leal@outlook.com>
2020-12-09 17:51:49 -0300

    Não consegui criar um algoritmo para ordenar, está é a minha primeira experiencia com programação, ainda estou aprendendo alguns conceitos que para o restante já estão claros, por isso ajustei o codigo do site para funcionar com o array preenchido pelo meu código e pedi ajuda do Ricardo para chamar os métodos na main.

--- BecaDotNet/Program.cs
+++ BecaDotNet/Program.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BecaDotNet
+﻿namespace BecaDotNet
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Hello world");
+            Algoritmos alg = new Algoritmos();
+            alg.Leitura();
+            alg.Bubblesort();
+            
         }
     }
 }
---
+++ BecaDotNet/algoritmos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BecaDotNet
+{
+    public class Algoritmos
+    {
+        int[] Numeros = new int[5] { 0, 0, 0, 0, 0 }; //armazena os números a serem ordenados
+
+        public void Leitura() //Faz a leitura dos valores a serem ordenados
+        {
+            Console.WriteLine("insira os numeros a serem ordenados: ");
+            Console.ReadLine();
+            for (int x = 0; x < Numeros.Length; x++)//salva no array os numeros a serem ordenados
+            {
+                Numeros[x] = int.Parse(Console.ReadLine());//entra os valores que devem ser armazenados no array
+            }
+        }
+
+        public void Bubblesort()
+                                
+        {
+            for (int j = 0; j <= Numeros.Length - 2; j++)
+            {
+                for (int i = 0; i <= Numeros.Length - 2; i++)
+                {
+                    if (Numeros[i] > Numeros[i + 1])
+                    {
+                        int temp = Numeros[i + 1];
+                        Numeros[i + 1] = Numeros[i];
+                        Numeros[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Números em ordem:");
+            foreach (int p in Numeros)
+                Console.Write(p + " ");
+            Console.Read();
+        }
+    }
+}
