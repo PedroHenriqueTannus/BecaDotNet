40613066a21ca5fb1ed961240ddb4811c4577dc6
Diego Prestes <diegoprestes.git@hotmail.com>
2020-12-09 16:10:48 -0300

    fear: bubblesort implementation

---
+++ BecaDotNet/Bubblesort.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BecaDotNet
+{
+    class Bubblesort
+    {
+        public void LerArray(int[] ordenado)
+        {
+            for (int i = 0; i < ordenado.Length; i++)
+            {
+                Console.Write($"{ordenado[i]} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("FEITO!");
+        }
+        public int[] BubbleExecuteCrescent(int[] desordem)
+        {
+            int mao = 0;
+
+            for (int i = 0; i < desordem.Length - 1; i++)
+            {
+                for (int j = 0; j < desordem.Length - 1; j++)
+                {
+                    if (desordem[j] > desordem[j + 1])
+                    {
+                        mao = desordem[j + 1];
+                        desordem[j + 1] = desordem[j];
+                        desordem[j] = mao;
+                    }
+                }
+            }
+            return desordem;
+        }
+
+        public int[] BubbleExecuteDecrescent(int[] desordem)
+        {
+            int mao = 0;
+
+            for (int i = desordem.Length - 1; i > 0; i--)
+            {
+                for (int j = desordem.Length - 1; j > 0; j--)
+                {
+                    if (desordem[j] > desordem[j - 1])
+                    {
+                        mao = desordem[j - 1];
+                        desordem[j - 1] = desordem[j];
+                        desordem[j] = mao;
+                    }
+                }
+            }
+            return desordem;
+        }
+    }
+}
--- BecaDotNet/Program.cs
+++ BecaDotNet/Program.cs
@@ -1,17 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace BecaDotNet
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int[] arr = { 78, 55, 45, 98, 13 };
+            Bubblesort bs = new Bubblesort();
+            //chamada Diego Prestes
+            int[] ordemCresc = bs.BubbleExecuteCrescent(arr);
+            bs.LerArray(ordemCresc);
+            //chamada Diego Prestes
+            int[] ordemDecresc = bs.BubbleExecuteDecrescent(arr);
+            bs.LerArray(ordemDecresc);
 
-            Console.WriteLine("Hello world");
+            Console.Read();
         }
     }
 }
